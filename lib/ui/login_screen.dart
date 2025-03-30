b636c9a8217be25f0be362125105087da728d754
Diego Minelli <45859999+diegominelli@users.noreply.github.com>
2025-03-30 20:07:00 -0300

    adicionando as imagens na tela de login

--- lib/ui/login_screen.dart
+++ lib/ui/login_screen.dart
@@ -10,42 +10,48 @@ class LoginScreen extends StatelessWidget {
         padding: const EdgeInsets.all(32),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
-            const Text(
-              "Sistema de Gestão de Contas",
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                fontSize: 32,
-              ),
-            ),
-            const SizedBox(height: 16),
-            const TextField(
-              decoration: InputDecoration(
-                label: Text("E-mail"),
-              ),
-            ),
-            const SizedBox(height: 16),
-            const TextField(
-              obscureText: true,
-              decoration: InputDecoration(
-                label: Text("Senha"),
-              ),
-            ),
-            const SizedBox(height: 32),
-            ElevatedButton(
-              onPressed: () {},
-              style: const ButtonStyle(
-                backgroundColor: WidgetStatePropertyAll(
-                  Color(0xFFFFA902),
+            Image.asset("assets/logo.png", width: 120),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                const SizedBox(height: 32),
+                const Text(
+                  "Sistema de Gestão de Contas",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 32,
+                  ),
                 ),
-              ),
-              child: const Text(
-                "Entrar",
-                style: TextStyle(
-                  color: Colors.black,
+                const SizedBox(height: 32),
+                const TextField(
+                  decoration: InputDecoration(
+                    label: Text("E-mail"),
+                  ),
                 ),
-              ),
+                const SizedBox(height: 16),
+                const TextField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    label: Text("Senha"),
+                  ),
+                ),
+                const SizedBox(height: 32),
+                ElevatedButton(
+                  onPressed: () {},
+                  style: const ButtonStyle(
+                    backgroundColor: WidgetStatePropertyAll(
+                      Color(0xFFFFA902),
+                    ),
+                  ),
+                  child: const Text(
+                    "Entrar",
+                    style: TextStyle(
+                      color: Colors.black,
+                    ),
+                  ),
+                ),
+              ],
             )
           ],
         ),
