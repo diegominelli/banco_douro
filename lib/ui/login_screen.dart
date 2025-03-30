5b9315b26fb304c92dbd17611b1be5fb33e157ab
Diego Minelli <45859999+diegominelli@users.noreply.github.com>
2025-03-30 20:15:27 -0300

    finalizando a tela de login usando o stack

--- lib/ui/login_screen.dart
+++ lib/ui/login_screen.dart
@@ -6,55 +6,65 @@ class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(32),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Image.asset("assets/logo.png", width: 120),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                const SizedBox(height: 32),
-                const Text(
-                  "Sistema de Gestão de Contas",
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontSize: 32,
-                  ),
-                ),
-                const SizedBox(height: 32),
-                const TextField(
-                  decoration: InputDecoration(
-                    label: Text("E-mail"),
-                  ),
-                ),
-                const SizedBox(height: 16),
-                const TextField(
-                  obscureText: true,
-                  decoration: InputDecoration(
-                    label: Text("Senha"),
-                  ),
-                ),
-                const SizedBox(height: 32),
-                ElevatedButton(
-                  onPressed: () {},
-                  style: const ButtonStyle(
-                    backgroundColor: WidgetStatePropertyAll(
-                      Color(0xFFFFA902),
+      body: Stack(
+        children: [
+          Image.asset("assets/banner.png"),
+          Align(
+            alignment: Alignment.bottomLeft,
+            child: Image.asset("assets/stars.png"),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(32),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                const SizedBox(height: 128),
+                Image.asset("assets/logo.png", width: 120),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    const SizedBox(height: 32),
+                    const Text(
+                      "Sistema de Gestão de Contas",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontSize: 32,
+                      ),
                     ),
-                  ),
-                  child: const Text(
-                    "Entrar",
-                    style: TextStyle(
-                      color: Colors.black,
+                    const SizedBox(height: 32),
+                    const TextField(
+                      decoration: InputDecoration(
+                        label: Text("E-mail"),
+                      ),
                     ),
-                  ),
-                ),
+                    const SizedBox(height: 16),
+                    const TextField(
+                      obscureText: true,
+                      decoration: InputDecoration(
+                        label: Text("Senha"),
+                      ),
+                    ),
+                    const SizedBox(height: 32),
+                    ElevatedButton(
+                      onPressed: () {},
+                      style: const ButtonStyle(
+                        backgroundColor: WidgetStatePropertyAll(
+                          Color(0xFFFFA902),
+                        ),
+                      ),
+                      child: const Text(
+                        "Entrar",
+                        style: TextStyle(
+                          color: Colors.black,
+                        ),
+                      ),
+                    ),
+                  ],
+                )
               ],
-            )
-          ],
-        ),
+            ),
+          ),
+        ],
       ),
     );
   }
